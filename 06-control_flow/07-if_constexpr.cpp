635f80a2f58a8ae27e68202276a5ee149465f1ee
Samkiroko <kirokodev@gmail.com>
2023-04-18 13:11:26 +0300

    If constexpr

---
+++ 06-control_flow/07-if_constexpr.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  constexpr bool condition(false);
+
+  if constexpr (condition)
+  {
+    cout << "Condition is true" << endl;
+  }
+  else
+  {
+    cout << "Condition is not true" << endl;
+  }
+
+  return 0;
+}
