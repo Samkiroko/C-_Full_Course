dbe91c7408a35e25953ac56fe601123bfbeb888c
Samkiroko <kirokodev@gmail.com>
2023-04-23 16:44:52 +0300

    pointer swap

---
+++ .history/09-pointer/07-swaping_arrays_20230423163806.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int arr0[]{1, 2, 3, 4, 5};
+  int arr1[]{6, 7, 8, 9, 10};
+
+  int *p_arr0 = arr0;
+  int *p_arr1 = arr1;
+
+  int *temp;
+
+  return 0;
+}
---
+++ .history/09-pointer/07-swaping_arrays_20230423164118.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int arr0[]{1, 2, 3, 4, 5};
+  int arr1[]{6, 7, 8, 9, 10};
+
+  int *p_arr0 = arr0;
+  int *p_arr1 = arr1;
+
+  int *temp;
+
+  temp = p_arr1;
+  p_arr1 = p_arr0;
+  p_arr0 = temp;
+
+  cout << "arr0 value : " << *arr0 << endl;
+  cout << "arr1 value : " << *arr1 << endl;
+
+  return 0;
+}
